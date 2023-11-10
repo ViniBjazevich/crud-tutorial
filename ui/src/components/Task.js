f709180d5a3b7222240431a5c01ba13bb3e42a68
Vini Bjazevich <vinibjazevich97@gmail.com>
2023-11-09 16:45:02 -0800

    feat: added checkbox funtionality

--- ui/src/components/Task.js
+++ ui/src/components/Task.js
@@ -6,37 +6,48 @@ import DialogTitle from "@mui/material/DialogTitle";
 import CheckIcon from "@mui/icons-material/Check";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import Checkbox from "@mui/material/Checkbox";
+import classnames from "classnames";
 
 export const Task = ({ name }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
 
   return (
-    <div>
-      <div className="task">
+    <div className="task">
+      <div
+        className={classnames("flex", {
+          done: isComplete,
+        })}
+      >
+        <Checkbox
+          checked={isComplete}
+          onChange={() => setIsComplete((prev) => !prev)}
+        />
         <Typography variant="h4">{name}</Typography>
-        <div className="taskButtons">
-          <Button variant="contained" onClick={() => setIsDialogOpen(true)}>
-            <EditIcon />
-          </Button>
-          <Button variant="contained" color="error">
-            <DeleteIcon />
+      </div>
+      <div className="taskButtons">
+        <Button variant="contained" onClick={() => setIsDialogOpen(true)}>
+          <EditIcon />
+        </Button>
+        <Button variant="contained" color="error">
+          <DeleteIcon />
+        </Button>
+      </div>
+      <Dialog open={isDialogOpen}>
+        <DialogTitle>Edit Task</DialogTitle>
+        <div className="dialog">
+          <TextField
+            size="small"
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+          />
+          <Button variant="contained" onClick={() => setIsDialogOpen(false)}>
+            <CheckIcon />
           </Button>
         </div>
-        <Dialog open={isDialogOpen}>
-          <DialogTitle>Edit Task</DialogTitle>
-          <div className="dialog">
-            <TextField
-              size="small"
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-            />
-            <Button variant="contained" onClick={() => setIsDialogOpen(false)}>
-              <CheckIcon />
-            </Button>
-          </div>
-        </Dialog>
-      </div>
+      </Dialog>
     </div>
   );
 };
