e449cb089eebe12a0f3f3d81afe51465415275df
Vini Bjazevich <vinibjazevich97@gmail.com>
2023-11-14 17:25:34 -0800

    fix: removed use of cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import serverless from "serverless-http";
 import { createTask, fetchTasks, updateTask, deleteTask } from "./tasks.js";
-import cors from "cors";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
